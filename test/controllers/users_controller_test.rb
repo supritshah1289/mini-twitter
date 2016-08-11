5da64ae7de889740d38b26d83f4f047563d9de08
supritkumar shah <suprit7@gmail.com>
2016-08-11 11:00:05 -0400

    make a basic user model including secure password and validating password

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
   test "should get new" do
-    get sign_path
+    get signup_path
     assert_response :success
   end
 
