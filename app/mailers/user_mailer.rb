7140a1d4c495211c249921df70e7292e9da51918
supritkumar shah <suprit7@gmail.com>
2016-08-12 10:29:34 -0400

    Add password reset

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -10,14 +10,21 @@ class UserMailer < ApplicationMailer
     mail to: user.email, subject: "Account activation"
   end
 
+
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
   #
   #   en.user_mailer.password_reset.subject
   #
-  def password_reset
-    @greeting = "Hi"
+  # def password_reset
+  #   @greeting = "Hi"
+  #   mail to: "to@example.org"
+  # end
 
-    mail to: "to@example.org"
+  #password reset
+  def password_reset(user)
+    @user = user
+    mail to: user.email, subject: "Password reset"
   end
+
 end
