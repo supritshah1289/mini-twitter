b4c4572f77116b387fac736bc364c8a278881b00
supritkumar shah <suprit7@gmail.com>
2016-08-11 23:16:25 -0400

    add account activations

--- app/models/user.rb
+++ app/models/user.rb
@@ -42,6 +42,7 @@ class User < ApplicationRecord
   def activate
     update_attribute(:activated,    true)
     update_attribute(:activated_at, Time.zone.now)
+    update_columns(activated: true, activated_at: Time.zone.now)
   end
 
   # Sends activation email.
